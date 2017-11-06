ba65c7d883c081d5b05c49fb0ad9da79bf305ea2
zhoujunpeng <zjp0432@163.com>
2017-11-06 17:35:42 +0800

    basic features are completed

---
+++ README.md
@@ -0,0 +1,5 @@
+# boi-transpiler
+[![license](https://img.shields.io/github/license/boijs/boi.svg?style=plastic)](https://github.com/boijs/boi/blob/master/LICENSE)
+[![npm](https://img.shields.io/npm/v/boi-transpiler.svg?style=plastic)](https://www.npmjs.com/package/boi-transpiler)
+
+tranpile boi configuration to webpack configuration  & dependencies
--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const _ = require('lodash');
-const Path = require('path');
-const Shell = require('shelljs');
-const WebpackMerge = require('webpack-merge');
-const TranspileEntry = require('./lib/entry.js');
-const TranspileOutput = require('./lib/output.js');
+const _                        = require('lodash');
+const Path                     = require('path');
+const Shell                    = require('shelljs');
+const WebpackMerge             = require('webpack-merge');
+const TranspileEntry           = require('./lib/entry.js');
+const TranspileOutput          = require('./lib/output.js');
+const TranspileBoiPlugins      = require('./lib/pluginsOfBoi.js');
 const TranspileModuleAndPlugin = require('./lib/moduleAndPlugins.js');
-const TranspileBoiPlugins = require('./lib/boiPlugins.js');
 
 /**
  * @module boi/transpiler
- * @param {Object} options boi configuration
- * @param {Boolean} isDevelopment indicate whether is development environment
- * @return {Object} webpack configuration and dependencies
+ * @param  {Object}  options boi configuration
+ * @param  {boolean} installPluginsAndDeps whether or not install boi plugins and dependencies
+ * @return {Object}  webpack configuration and dependencies
  */
-module.exports = function(options,isDevelopment){
-  const ModuleAndPlugins = TranspileModuleAndPlugin(options,isDevelopment);
-  const EntryAndPlugins = TranspileEntry(options,isDevelopment);
-  const Output = TranspileOutput(options,isDevelopment);
-  const WebpackConfOfBoiPlugins = TranspileBoiPlugins(options.plugins);
+module.exports = function (options, installPluginsAndDeps = false) {
+
+  const IsDevelopment           = process.env.BOI_ENV === 'development' ? true : false;
+  
+  const EntryAndPlugins         = TranspileEntry(options, IsDevelopment);
+  const Output                  = TranspileOutput(options, IsDevelopment);
+  const WebpackConfOfBoiPlugins = TranspileBoiPlugins(options.plugins, installPluginsAndDeps);
+  const ModuleAndPlugins        = TranspileModuleAndPlugin(options, IsDevelopment, installPluginsAndDeps);
+
   /**
-   * global node_modules path,
    * By default global modules can not be resolved on nvm environment
-   * @constant 
+   * @constant NpmRootPath global node_modules path
    * @see https://github.com/creationix/nvm/pull/97
    */
-  const NpmRootPath = _.trim(Shell.exec('npm root -g', { silent: true }).stdout);
+  const NpmRootPath = _.trim(Shell.exec('npm root -g', {
+    silent: true
+  }).stdout);
 
-  // 全局安装boi的node_modules目录
+  // node_modules path under global boi module
   const BoiModulesPath = Path.posix.join(NpmRootPath, 'boi/node_modules');
 
   return {
@@ -37,9 +42,9 @@ module.exports = function(options,isDevelopment){
       plugins: EntryAndPlugins.plugins,
       resolveLoader: {
         modules: [
-          // 构建工具自身的模块目录
+          // node_modules path of the util
           Path.posix.join(__dirname, './node_modules'),
-          // 项目自身的模块目录
+          // node_modules path of specific project
           Path.posix.join(process.cwd(), 'node_modules'),
           NpmRootPath,
           BoiModulesPath
@@ -54,23 +59,23 @@ module.exports = function(options,isDevelopment){
         ],
         // disable symlinks
         symlinks: false,
-        // '@' indicate the root path of source files
+        // '@' indicate the root path of source directory
         alias: {
-          '@': Path.posix.join(process.cwd(),options.compile.basic.source)
+          '@': Path.posix.join(process.cwd(), options.compile.basic.source)
         }
       },
-      // 开发环境使用eval-source-map提高重构建速度
-      devtool: isDevelopment ? 'eval-source-map' : false,
+      // enable eval-source-map on devepelopment environment
+      devtool: IsDevelopment ? 'eval-source-map' : false,
       // 性能指标
-      performance: isDevelopment ? {} : Object.assign({
+      performance: IsDevelopment ? {} : Object.assign({
         // 超标文件提示错误
         hints: 'warning',
-        // 入口文件最大不超过150kb
+        // the maxsize of each chunks shall not exceed 150kb
         maxEntrypointSize: 150000,
-        // 所有类型文件最大不超过200kb
+        // the maxsize of all kind of resource shall not exceed 200kb
         maxAssetSize: 200000
-      },options.compile.basic.limit)
-    },ModuleAndPlugins.webpackConf,...WebpackConfOfBoiPlugins.webpackConf),
+      }, options.compile.basic.limit)
+    }, ModuleAndPlugins.webpackConf, ...WebpackConfOfBoiPlugins.webpackConf),
     dependencies: ModuleAndPlugins.dependencies.concat([...WebpackConfOfBoiPlugins.dependencies])
-  }
-} 
+  };
+};
